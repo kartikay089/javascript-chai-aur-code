3dee465d1bfced6a1af9d2616dcf2fabea7d8caf
kartikay <kartikay1077@gmail.com>
2024-04-01 23:00:26 +0530

    scripts.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let score = 33;
+
+console.log(score, typeof score);
+
+let myYoutubeName = "kartikaydotcom";
+
+let anotherName = myYoutubeName;
+
+anotherName = "chaiaurcode";
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let user = {
+  user: "kartikayTeotia",
+  upi: "123@ybl",
+};
+
+console.log(user);
