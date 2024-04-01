6bbe71b988ff09da2f77f4278617dcc5487e89a2
kartikay <kartikay1077@gmail.com>
2024-04-01 13:34:46 +0530

    numbers notes

---
+++ 03_numbers.js
@@ -0,0 +1,8 @@
+const score = 500;
+
+// console.log(score.toLocaleString().slice(0, 2).indexOf(5));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
